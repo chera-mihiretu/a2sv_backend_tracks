b406fc60c86ae4fe846340d1c55cdfe6c8ea9cf7
chera-mihiretu <cheramihiretucm@gmail.com>
2024-09-24 10:02:33 -0400

    Variable scope

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+// this variables has package scope
+// meaning they are availeble through out the package
+// and must be declared by var
+var name, age, gender = "Chera Mihiretu", 21, "M"
+
+var one, two, three int // all of them are going to declared as 0
+
+var isTrue bool // intialized as false
+
+var myString string // going to be initialized to ''
+
 func main() {
-	fmt.Println("Hellow World")
+	fmt.Println(name, age, gender)
+	fmt.Println(one, two, three)
+	fmt.Println(isTrue)
+	fmt.Println(myString)
+
+	var unval = "This is local variable"
+
+	fmt.Println(unval)
 }
