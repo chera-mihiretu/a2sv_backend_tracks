239205c43a0f46791b94c55916b2786343dbd7ab
chera-mihiretu <cheramihiretucm@gmail.com>
2024-12-20 22:02:44 +0300

    variables

---
+++ go_basics/variables/variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var one, two bool
+var int_one, int_two int
+var char_one, char_two string
+var byte_one, byte_two byte
+
+func main() {
+	fmt.Println(one, two)
+	fmt.Println(int_one, int_two)
+	fmt.Println(char_one, char_two)
+	fmt.Println(byte_one, byte_two)
+}
