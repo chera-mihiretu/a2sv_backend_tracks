9747e197e7aa9775c0a94e82fa59630e99e67011
chera-mihiretu <cheramihiretucm@gmail.com>
2024-12-31 20:27:23 +0300

    setting up the mongo db cluster

--- a2sv_tasks/task4.task_manager/main.go
+++ a2sv_tasks/task4.task_manager/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"github/chera/task_manager/router"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	router.Routers()
 }
