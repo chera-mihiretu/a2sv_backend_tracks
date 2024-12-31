abeaa3d73b2182476a7e85099e62e9c635a28579
chera-mihiretu <cheramihiretucm@gmail.com>
2024-12-31 11:04:16 +0300

    fixing interface implementation

--- a2sv_tasks/task4.task_manager/data/task_service.go
+++ a2sv_tasks/task4.task_manager/data/task_service.go
@@ -2,10 +2,41 @@ package services
 
 import "github/chera/task_manager/models"
 
-type TaskServices interface {
+type TaskManager interface {
+	OpennFile() error
 	AddTask() error
 	RemoveTask() error
 	UpdateTask() error
 	GetTask() models.Taks
 	GetTasks() []models.Taks
+}
+
+// TaskService struct that open file
+type TaskService struct {
+	AllTaks map[int]models.Taks
+}
+
+// TaskService struct that add new task
+func (t *TaskService) AddTask() error {
+	return nil
+}
+
+// TaskService struct that remove task
+func (t *TaskService) RemoveTask() error {
+	return nil
+}
+
+// TaskService struct that update task
+func (t *TaskService) UpdateTask() error {
+	return nil
+}
+
+// TaskService struct that get task
+func (t *TaskService) GetTask() models.Taks {
+	return models.Taks{}
+}
+
+// TaskService struct that get tasks
+func (t *TaskService) GetTasks() []models.Taks {
+	return []models.Taks{}
 }
