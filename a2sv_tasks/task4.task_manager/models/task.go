8d221d4c70f3769d810886bdca15a014707e86ba
chera-mihiretu <cheramihiretucm@gmail.com>
2024-12-31 10:56:02 +0300

    creating a task model

---
+++ a2sv_tasks/task4.task_manager/models/task.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type Taks struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
---
+++ a2sv_tasks/task4.task_manager/router/router.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func routers(c *gin.Context) {
+	router := gin.Default()
+}
